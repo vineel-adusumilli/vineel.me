aa56953360e158205d30f8909af056c2de832a84
Vineel Adusumilli <vineel@Vineels-MacBook.local>
2012-07-03 00:01:17 -0700

    current link stays selected

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+pages = ['projects', 'experiments', 'articles', 'about', 'contact']
+
 @app.route('/')
 def index():
-  return render_template('index.html')
+  return render_template('index.html', pages=pages, current='index')
+
+@app.route('/<page>/')
+def go_to(page):
+  if page in pages:
+    return render_template('index.html', title=page, pages=pages, current=page)
+  else:
+    abort(404)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
