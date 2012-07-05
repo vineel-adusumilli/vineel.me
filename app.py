3cfdd49de3f9cbad3e48fb18bc21242c849905ca
Vineel Adusumilli <vineel@vineels-macbook.gateway.2wire.net>
2012-07-04 18:14:31 -0700

    added error pages

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 app = Flask(__name__)
 
 pages = ['projects', 'experiments', 'articles', 'about', 'contact']
@@ -10,10 +10,18 @@ def index():
 @app.route('/<page>/')
 def go_to(page):
   if page in pages:
-    return render_template('%s.html' % page, title=page, pages=pages, current=page)
+    return render_template('pages/%s.html' % page, title=page, pages=pages, current=page)
   else:
     abort(404)
 
+@app.errorhandler(404)
+def page_not_found(e):
+  return render_template('errors/404.html', title='404', pages=pages), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+  return render_template('errors/500.html', title='500', pages=pages), 500
+
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+  app.run(host='0.0.0.0', debug=False)
   
