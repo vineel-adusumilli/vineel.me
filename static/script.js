e8e0044c3df1dd8f74889df6a9543cff1429b484
Vineel Adusumilli <vineel@adusumilli.org>
2012-07-19 19:43:11 -0700

    shadow not as stronk

--- static/script.js
+++ static/script.js
@@ -3,11 +3,11 @@ $(document).ready(function() {
     function() {
       var img = $(this).find("img.thumbnail")[0];
       img.src = img.src.replace("-off", "-on");
-      $(img).css("box-shadow", "0 0 6px 6px rgb(136, 136, 136)");
+      $(img).css("box-shadow", "0px 0px 10px 4px rgb(136, 136, 136)");
     },
     function() {
       var img = $(this).find("img.thumbnail")[0];
       img.src = img.src.replace("-on", "-off");
-      $(img).css("box-shadow", "0 0 5px 5px rgb(136, 136, 136)");
+      $(img).css("box-shadow", "0px 0px 10px 2px rgb(136, 136, 136)");
   });
 }); 
