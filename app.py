b63acb4b4ac0c41de5db9a7601c76a1c3405b07e
Vineel Adusumilli <vineel@adusumilli.org>
2012-08-03 00:17:24 -0700

    beautified

--- app.py
+++ app.py
@@ -1,27 +1,77 @@
+import ConfigParser
 from flask import Flask, render_template, abort
 app = Flask(__name__)
 
-pages = ['projects', 'experiments', 'articles', 'about', 'contact']
+pages = []
+sub_pages = {}
 
 @app.route('/')
 def index():
-  return render_template('index.html', pages=pages, current='index')
+  recent = []
+  for (page, links) in sub_pages.items():
+    if links:
+      recent.append((page, links[0]))
+  return render_template('index.html', pages=pages, links=recent, current='index')
 
 @app.route('/<page>/')
 def go_to(page):
+  page = linkname(page)
   if page in pages:
-    return render_template('pages/%s.html' % page, title=page, pages=pages, current=page)
+    print sub_pages[page]
+    return render_template('pages/%s.html' % page, pages=pages, links=sub_pages[page], current=page)
   else:
     abort(404)
 
 @app.errorhandler(404)
 def page_not_found(e):
-  return render_template('errors/404.html', title='404', pages=pages), 404
+  return render_template('errors/404.html', pages=pages), 404
 
 @app.errorhandler(500)
 def internal_server_error(e):
-  return render_template('errors/500.html', title='500', pages=pages), 500
+  return render_template('errors/500.html', pages=pages), 500
+
+# converts Title into link version
+# example: "O' Green World" would become "o-green-world"
+def linkname(title):
+  title = ''.join(c for c in title if c.isalnum() or c.isspace())
+  return title.lower().replace(' ', '-')
+
+def link(title, page):
+  return '/%s/%s/' % (page, linkname(title))
+
+def thumbnail_on(title, page):
+  return '/static/%s/%s/thumbnail-on.jpg' % (page, linkname(title)) 
+
+def thumbnail_off(title, page):
+  return '/static/%s/%s/thumbnail-off.jpg' % (page, linkname(title)) 
+
+def icon_on(service):
+  return '/static/images/%s-on.png' % linkname(service)
+
+def icon_off(service):
+  return '/static/images/%s-off.png' % linkname(service)
+
+# add custom filters to jinja2
+app.jinja_env.filters['linkname']      = linkname
+app.jinja_env.filters['link']          = link
+app.jinja_env.filters['thumbnail_on']  = thumbnail_on
+app.jinja_env.filters['thumbnail_off'] = thumbnail_off
+app.jinja_env.filters['icon_on']       = icon_on
+app.jinja_env.filters['icon_off']      = icon_off
+
+def init():
+  cfg = ConfigParser.ConfigParser()
+  cfg.read('pages.cfg')
+  
+  for (_, page) in cfg.items('pages'):
+    pages.append(page)
+    
+    links = []
+    for (_, link) in cfg.items(page):
+      links.insert(0, link)
+    sub_pages[page] = links
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=False)
+  init()
+  app.run(host='0.0.0.0', debug=True)
   
--- static/script.js
+++ static/script.js
@@ -1,13 +1,27 @@
 $(document).ready(function() {
+  $("img.rollover").hover(
+    function() {
+      this.src = this.src.replace("-off", "-on");
+    },
+    function() {
+      this.src = this.src.replace("-on", "-off");
+    }
+  );
+
   $("div.link").hover(
     function() {
       var img = $(this).find("img.thumbnail")[0];
       img.src = img.src.replace("-off", "-on");
+      $(img).css("-moz-box-shadow", "0px 0px 10px 4px rgb(136, 136, 136)");
+      $(img).css("-webkit-box-shadow", "0px 0px 10px 4px rgb(136, 136, 136)");
       $(img).css("box-shadow", "0px 0px 10px 4px rgb(136, 136, 136)");
     },
     function() {
       var img = $(this).find("img.thumbnail")[0];
       img.src = img.src.replace("-on", "-off");
+      $(img).css("-moz-box-shadow", "0px 0px 10px 2px rgb(136, 136, 136)");
+      $(img).css("-webkit-box-shadow", "0px 0px 10px 2px rgb(136, 136, 136)");
       $(img).css("box-shadow", "0px 0px 10px 2px rgb(136, 136, 136)");
-  });
+    }
+  );
 }); 
