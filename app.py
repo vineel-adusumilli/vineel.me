3fdb31700fac2693bb28930fe165121ce86045e0
Vineel Adusumilli <vineel@adusumilli.org>
2013-12-24 00:00:26 -0800

    Turned off debug flag.

--- app.py
+++ app.py
@@ -355,5 +355,5 @@ def image_delete(image, post):
 
 if __name__ == '__main__':
   port = int(os.environ.get('PORT', 5000))
-  app.run(host='0.0.0.0', port=port, debug=True)
+  app.run(host='0.0.0.0', port=port, debug=False)
   
